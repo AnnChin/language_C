2b7b71d3d837923de132812ad8baca4eb23546d7
AnnChin <nyusechca@mail.ru>
2023-09-07 18:58:57 +0300

    done home13

---
+++ seminar2hometask13/Program.cs
@@ -0,0 +1,35 @@
+﻿//Напишите программу, которая выводит только третью цифру заданного числа или сообщает, что третьей цифры нет
+
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a < 100)
+{
+    Console.WriteLine("There is no such number");
+}
+else if (a < 1000)
+{
+    int b = a % 10;
+    Console.WriteLine(b);
+}
+else if (a <10000)
+{
+    int b = a % 100/10;
+    Console.WriteLine(b);
+}
+else if (a <100000)
+{
+    int b = a % 1000/100;
+    Console.WriteLine(b);
+}
+else if (a <1000000)
+{
+    int b = a % 10000/1000;
+    Console.WriteLine(b);
+}
+else if (a <10000000)
+{
+    int b = a % 100000/10000;
+    Console.WriteLine(b);
+}
