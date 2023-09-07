4db593813976ca3770ab109a635568ca02eff4a4
AnnChin <nyusechca@mail.ru>
2023-09-07 14:28:04 +0300

    done home1

---
+++ seminar1hometask1/Program.cs
@@ -0,0 +1,17 @@
+﻿//напишите прошрамму которая на вход принимает 2 числа и выдает какое число больше, а какое меньше
+
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите первое число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a>b)
+{
+    Console.WriteLine($"max = {a}");
+    Console.WriteLine($"min = {b}");
+}
+else
+{
+    Console.WriteLine($"max = {b}");
+    Console.WriteLine($"min = {a}");
+}
