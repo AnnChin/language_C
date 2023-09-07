f00b4260d877a4c2317a588d325b2082cf6ec9fb
AnnChin <nyusechca@mail.ru>
2023-09-07 17:45:23 +0300

    add seminar2task1

---
+++ seminar2task1/Program.cs
@@ -0,0 +1,35 @@
+﻿// int x = 18;
+// while (x > 5)
+// {
+//     Console.WriteLine(x);
+//     x = x - 1;
+// }
+
+// for (int i = 0; i < 88; i = i + 3)
+// {
+//     Console.WriteLine(i);
+// }
+
+// // double y = Convert.ToDouble(Console.Readline());
+
+// double z = Math.Abs(-12.4); //12.4
+
+// double a = Math.Round(20.5417389, 2); //20.54
+
+// double b = Math.Sqrt(16); // 4
+
+// double c = Math.Pow(3, 2); //9
+
+for (int i = 0; i < 10; i++)
+{
+    int x = new Random().Next(10, 100);
+    if (x % 10 > x / 10)
+    {
+        Console.WriteLine(x % 10);
+    }
+    else
+    {
+        Console.WriteLine(x / 10);
+    }
+}
+
