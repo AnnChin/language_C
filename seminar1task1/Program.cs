e0475d8acb5a79c8ef7fc133e5bd2abdb2e0a8a4
AnnChin <nyusechca@mail.ru>
2023-09-07 12:22:15 +0300

    add task1

---
+++ seminar1task1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Console.Write("Введите имя: ");
+
+// string nameUser = Console.ReadLine();
+
+// Console.Write($"Ваше имя - {nameUser}");
+
+Console.Write("Введите число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"Ваше имя - {number + 10}");
