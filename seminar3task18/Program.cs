eb90338a48b1ceaeafdc8d6b706ec3528123239e
AnnChin <nyusechca@mail.ru>
2023-09-12 16:16:37 +0300

    done task18

---
+++ seminar3task18/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая по заданному номеру четверти,
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+
+Console.WriteLine("Insert number quarter plane: ");
+int quarter = Convert.ToInt32(Console.ReadLine());
+
+if (quarter == 1) Console.WriteLine("x = [1;1000], y = [1:1000]");
+else if (quarter == 2) Console.WriteLine("x = [-1;-1000], y = [1;1000]");
+else if (quarter == 3) Console.WriteLine("x = [-1;-1000], y = [-1;-1000]");
+else if (quarter == 4) Console.WriteLine("x = [1;1000], y = [-1;-1000]");
+else Console.WriteLine("does not exist");
