fd1f204fbc58bfd3d481675d4db4bea9c50a4982
AnnChin <nyusechca@mail.ru>
2023-09-12 20:49:04 +0300

    done seminar4hometask29

---
+++ seminar4hometask29/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран
+
+int n = 8;
+int[] array = new int[n];
+Random rnd = new Random();
+Console.Write("[");
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rnd.Next(0,100);
+    Console.Write($"{array[i]}, ");
+}
+Console.Write("]");
