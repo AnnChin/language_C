80d948454be77935c2403fa7ea2ee8d28e497465
AnnChin <nyusechca@mail.ru>
2023-09-12 16:09:35 +0300

    done task1

---
+++ seminar3task1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход координаты точки (X и Y),
+// причем X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости,
+// в которой находится эта точка.
+
+Console.WriteLine("Insert x coordinate: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Insert y coordinate: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x > 0 && y > 0) Console.WriteLine("your coordinates in 1 quarter plane");
+else if (x < 0 && y > 0) Console.WriteLine("your coordinates in 2 quarter plane");
+else if (x < 0 && y < 0) Console.WriteLine("your coordinates in 3 quarter plane");
+else if (x > 0 && y < 0) Console.WriteLine("your coordinates in 4 quarter plane");
+   
