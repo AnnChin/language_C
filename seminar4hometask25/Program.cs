72a0f55b1cfb04060e5a7d2a6ed81ca046b5feff
AnnChin <nyusechca@mail.ru>
2023-09-12 20:25:25 +0300

    done seminar4hometask25

---
+++ seminar4hometask25/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите цикл, который принимает на вход два числа (А,В)
+// и возводит число А в натуральную степень В
+
+double Degree(int a, int b)
+{
+    double n = Math.Pow(a, b);
+    return n;
+}
+
+Console.Write("Insert num 1: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Insert num 2: ");
+int B = Convert.ToInt32(Console.ReadLine());
+Console.Write(Degree(A, B));
