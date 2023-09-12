2a233404e0f306b73c8203a48e236d7fbe2f20ec
AnnChin <nyusechca@mail.ru>
2023-09-12 19:05:32 +0300

    add seminar4task1

---
+++ seminar4task1/Program.cs
@@ -0,0 +1,26 @@
+﻿// void SayLang(string lang)
+// {
+//     Console.WriteLine($"Lang {lang}!!!");
+// }
+
+// SayLang("kjhvb");
+// SayLang("rtgb");
+// SayLang("qasd");
+
+
+int SayMax(int num1, int num2)
+{
+    if (num1 > num2)
+    {
+        num1 = num1 + 10;
+        return num1 + 10;
+    }
+    else
+    {
+        num2 = num2 * 2;
+        return num2 * 2;
+    }
+}
+Console.WriteLine($"{SayMax(5,3)}");
+int x1 = SayMax(2,7);
+Console.WriteLine(x1);
