83562c5416b12f1b64a69e46604f3727b0833b31
AnnChin <nyusechca@mail.ru>
2023-09-12 20:08:00 +0300

    done seminar4task26

---
+++ seminar4task26/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+int Leng(string numb)
+{
+    int NumLength = numb.Length;
+    return NumLength;
+}
+
+Console.Write("Insert number: ");
+string i = Console.ReadLine();
+Console.WriteLine(Leng(i));
+
+// for (int i = 0; i < length; i++)
