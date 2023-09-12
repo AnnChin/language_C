61b6469e25e2baaf9b6f7f54f16fe263f97027e4
AnnChin <nyusechca@mail.ru>
2023-09-12 19:55:37 +0300

    done seminar4task28

---
+++ seminar4task28/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число N
+//  и выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+double Factorial(int n)
+{
+    if (n == 1) return 1;
+    else return n * Factorial(n - 1);
+}
+Console.Write("Insert number: ");
+int i = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Factorial(i));
