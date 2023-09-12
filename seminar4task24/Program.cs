41052f3df320ef283edb1c1f274aa62362657a96
AnnChin <nyusechca@mail.ru>
2023-09-12 19:16:21 +0300

    done seminar4task24

--- seminar4task24/Program.cs
+++ seminar4task24/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число (А)
+// и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+Console.Write("Insert number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+
+for (int i = 0; i <= n; i++)
+{
+    result = i + result;
+}
+Console.Write($"Sum of numbers from 1 to {n} = {result}");
