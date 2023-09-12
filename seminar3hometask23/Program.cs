256e6590f255bdeb2d6f9ba5177ba824c5e52c16
AnnChin <nyusechca@mail.ru>
2023-09-12 16:54:58 +0300

    done home23

---
+++ seminar3hometask23/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдает таблицу кубов чисел
+// от 1 до N.
+
+Console.Write("Insert number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= num; i++)
+{
+    Console.WriteLine($"{i} ^ 3 = {Math.Pow(i,3)}");
+}
