4c4011c802b80d3f1024a233dc5bf471bf44250a
AnnChin <nyusechca@mail.ru>
2023-09-13 18:19:27 +0300

    done seminar6task1

---
+++ seminar6task1/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] num = { 1, 2, 3, 4, 5, 6 };
+int[,] nums = { { 1, 2, 3 }, { 4, 5, 6 } };
+int[,] num2 = new int [2,3];
+
+for (int i = 0; i < nums.GetLength(0); i++)
+{
+    for (int j = 0; j < nums.GetLength(1); j++)
+    {
+        Console.Write($"{nums[i,j]} ");
+    }
+    Console.WriteLine();      
+}
