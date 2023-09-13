8c970938cd961254188c7a0bed45aa3c34914811
AnnChin <nyusechca@mail.ru>
2023-09-13 14:54:07 +0300

    added seminar5task32

--- seminar5task31/Program.cs
+++ seminar5task31/Program.cs
@@ -3,14 +3,13 @@
 // Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] 
 // сумма положительных чисел равна 29, сумма отрицательных равна -20.
 
-int n = 12;
-int[] array = new int[n];
-Random rnd = new Random();
+int[] array = new int[12];
 int sumPlus = 0;
 int sumMinus = 0;
 
 for (int i = 0; i < array.Length; i++)
 {
+    Random rnd = new Random();
     array[i] = rnd.Next(-9, 10);
     Console.Write($"{array[i]}, ");
     if (array[i] > 0) sumPlus = sumPlus + array[i];
