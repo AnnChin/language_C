ef609a25a4876f9204602292f17e9748778db413
AnnChin <nyusechca@mail.ru>
2023-09-13 15:40:18 +0300

    done seminar5hometask34

---
+++ seminar5hometask34/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задайте массив заполненный случайными положительными трехзначными числами.
+// Напишите программу, которая покажет количество четных чисел в массиве
+
+
+// функция массива
+int[] Massive()
+{
+    int n = 10;
+    int[] arr = new int[n];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(100, 1000);
+    }
+    return arr;
+}
+
+// функция печати массива
+void PrintMassive(int[] Mass)
+{
+    for (int i = 0; i < Mass.Length; i++)
+    {
+        Console.Write($"{Mass[i]}, ");
+    }
+    Console.WriteLine();
+}
+
+// функция счетчика
+int Counted(int[] con)
+{
+    int count = 0;
+    for (int i = 0; i < con.Length; i++)
+    {
+        if (con[i] % 2 == 0) count = count + 1;
+    }
+    return count;
+}
+
+int[] arr = Massive();
+PrintMassive(arr);
+Console.WriteLine($"Количество четных чисел = {Counted(arr)}");
