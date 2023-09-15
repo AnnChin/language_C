4502be694a2bad9e8adb9601d2ca97bc029e5713
AnnChin <nyusechca@mail.ru>
2023-09-15 13:00:34 +0300

    done seminar7task51

---
+++ seminar7task51/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали 
+// (с индексами (0,0); (1;1) и т.д.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+int LengthArray(string message)
+{
+    Console.Write(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+void RandomArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int SumDia(int[,] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (j == i)
+            {
+                sum = sum + arr[i, j];
+            }
+        }
+    }
+    return sum;
+}
+
+
+int lenColumn = LengthArray($"Задайте количество столбцов: ");
+int lenLine = LengthArray($"Задайте количество строк: ");
+int[,] array = new int[lenLine, lenColumn];
+RandomArray(array);
+PrintArray(array);
+Console.WriteLine($"Сумма эл-тов главной диагонали = {SumDia(array)}");
