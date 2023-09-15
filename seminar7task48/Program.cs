620a1a6e76a2ea543fe24394cdae4ef2eadec1c0
AnnChin <nyusechca@mail.ru>
2023-09-15 12:18:29 +0300

    done seminar7task48

---
+++ seminar7task48/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задайте двумерный массив размера m на n, каждый элемент в массиве находится
+// по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+int LengthArray(string message)
+{
+    Console.Write(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+void FillArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = i + j;
+        }
+    }
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int lenColumn = LengthArray($"Задайте количество столбцов: ");
+int lenLine = LengthArray($"Задайте количество строк: ");
+int[,] array = new int[lenLine, lenColumn];
+FillArray(array);
+PrintArray(array);
