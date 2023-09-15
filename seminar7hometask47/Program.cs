34212ecd1e8459107f984a89693f2dd2ba16e803
AnnChin <nyusechca@mail.ru>
2023-09-15 13:36:07 +0300

    done seminar7hometask47

---
+++ seminar7hometask47/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задайте двумерный массив размером m x n.
+// заполненный случайными вещественными числами.
+
+int LengthArray(string message)
+{
+    Console.Write(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+void RandomArray(double[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+        }
+    }
+}
+
+void PrintArray(double[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int lenColumn = LengthArray($"Задайте количество столбцов: ");
+int lenLine = LengthArray($"Задайте количество строк: ");
+double[,] array = new double[lenLine, lenColumn];
+RandomArray(array);
+PrintArray(array);
