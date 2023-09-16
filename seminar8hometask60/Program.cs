ca9476b2efc4795e75788c8fabe91ec70d9c8350
AnnChin <nyusechca@mail.ru>
2023-09-16 20:56:19 +0300

    done seminar8hometask60

---
+++ seminar8hometask60/Program.cs
@@ -0,0 +1,48 @@
+﻿// Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, 
+// добавляя индексы каждого элемента.
+
+int LengthArray(string message)
+{
+    Console.Write(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+void RandomArray(int[,,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int k = 0; k < arr.GetLength(2); k++)
+            {
+                arr[i, j, k] = new Random().Next(1, 50);
+            }
+        }
+    }
+}
+
+void PrintArray(int[,,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int k = 0; k < arr.GetLength(2); k++)
+            {
+                Console.Write($"arr[{i},{j},{k}]={arr[i, j, k]} ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+}
+
+int lenColumn = LengthArray($"Задайте количество столбцов массива: ");
+int lenLine = LengthArray($"Задайте количество строк массива: ");
+int lenDepth = LengthArray($"Задайте глубину массива: ");
+
+int[,,] arrA = new int[lenLine, lenColumn, lenDepth];
+RandomArray(arrA);
+PrintArray(arrA);
