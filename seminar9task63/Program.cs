9a4b27b93bfb628dbde7efee72ab29cdd8f6bf5d
AnnChin <nyusechca@mail.ru>
2023-09-17 12:59:02 +0300

    done seminar9task63

---
+++ seminar9task63/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте значение N.
+// Напишите программу, которая выведет все натуральные числа
+// в промежутке от 1 до N.
+
+Console.Write($"Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void Order(int x)
+{
+    if (x == 0)
+    {
+        return;
+    }
+    Order(x-1);
+    Console.Write($"{x}, ");
+}
+
+Order(n);
