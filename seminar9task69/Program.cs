3e4bd8f67c1f8b2b1df1272d09bc3f603c09d084
AnnChin <nyusechca@mail.ru>
2023-09-17 13:33:05 +0300

    done seminar9task69

---
+++ seminar9task69/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая на входпринимает 2 числа A и B,
+// и возводит число А в целую степень В с помощью рекурсии.
+
+Console.Write($"Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Введите степень возведения: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+
+int PowNum(int x, int y)
+{
+    if (y == 0)
+    {
+        return 1;
+    }
+    else return x * PowNum(x, y - 1);
+
+}
+
+Console.Write(PowNum(n, m));
