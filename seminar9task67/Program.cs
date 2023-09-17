294c595b054e73a18d0332d34c5318d202f0ef4a
AnnChin <nyusechca@mail.ru>
2023-09-17 13:27:51 +0300

    done seminar9task67

---
+++ seminar9task67/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая будет
+// принимать на вход число и возвращать сумму его цифр
+
+Console.Write($"Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int SumNum(int x)
+{
+    if (x == 0) return 0;
+    else return x % 10 + SumNum(x / 10);
+}
+
+Console.WriteLine(SumNum(n));
