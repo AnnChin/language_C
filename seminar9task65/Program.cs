6f17e04d09bfb4477e158c40b427bfdd52141ae0
AnnChin <nyusechca@mail.ru>
2023-09-17 13:10:48 +0300

    done seminar9task65

---
+++ seminar9task65/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте значения M и N. Напишите программу,
+// которая выведет все натуральные числа от M до N.
+
+Console.Write($"Введите первое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Введите второе число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+void Order(int x, int y)
+{
+    if (x > y)
+    {
+        return;
+    }
+    Console.Write($"{x}, ");
+    Order(x + 1, y);
+}
+
+Order(m, n);
